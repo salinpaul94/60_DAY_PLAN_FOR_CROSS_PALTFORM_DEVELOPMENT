5277776afc16a958665dc353d35e06fb7866f2b0
salinpaul <salinpv@gmail.com>
2024-08-09 09:12:27 +0530

    day 4 task 8

--- test-roject/src/App.js
+++ test-roject/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import Welcome  from './components/welcome';
-
+import Product from './components/product';
 
 const user = {
   firstName: 'John',
@@ -11,6 +11,7 @@ const element = (
   <h1>
     <Welcome name="salin"/> 
     Hello, {user.firstName} {user.lastName}
+    <Product name="Gaming Chair" price={299}/>
   </h1>
 )
 

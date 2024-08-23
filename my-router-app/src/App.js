002a2e7127d4f3e41bbaf084301387a61675d851
salinpaul <salinpv@gmail.com>
2024-08-23 09:07:46 +0530

    day 6 task 1 example form

--- my-router-app/src/App.js
+++ my-router-app/src/App.js
@@ -1,21 +1,9 @@
 import './App.css';
-import RoutesPracticeTask20 from './componentsDay5/RoutesPracticeTask20';
+import SimpleFormTask1Example1 from './componentsDay6/FormsTask1Example1';
 
 function App() {
   return (
-    // <RouterTask14Example1/>
-    // <RouterTask14Example2/>
-    // <RouterTask15Example1/>
-    // <RouterTask15Example2/>
-    // <RouterTask16Example1/>
-    // <RouterTask16Example2/>
-    // <RouterTask17Example1/>
-    // <RouterParameterTask17Example2/>
-    // <UseNavigateTask18Example1/>
-    // <RouterParameterTask18Example2/>
-    // <RoutesPracticeTask19Example1/>
-    // <RouterPracticeTask19Example2/>
-    <RoutesPracticeTask20/>
+    <SimpleFormTask1Example1/>
   );
 }
 
---
+++ my-router-app/src/AppDay5.js
@@ -0,0 +1,22 @@
+import './App.css';
+import RoutesPracticeTask20 from './componentsDay5/RoutesPracticeTask20';
+
+function App() {
+  return (
+    // <RouterTask14Example1/>
+    // <RouterTask14Example2/>
+    // <RouterTask15Example1/>
+    // <RouterTask15Example2/>
+    // <RouterTask16Example1/>
+    // <RouterTask16Example2/>
+    // <RouterTask17Example1/>
+    // <RouterParameterTask17Example2/>
+    // <UseNavigateTask18Example1/>
+    // <RouterParameterTask18Example2/>
+    // <RoutesPracticeTask19Example1/>
+    // <RouterPracticeTask19Example2/>
+    <RoutesPracticeTask20/>
+  );
+}
+
+export default App;

2382ff3f7c3efee5b574c8aee3ddfc154a1d1ee6
salinpaul <salinpv@hotmail.com>
2024-08-28 22:51:42 +0530

    day 6 task 6 example 1

--- cilu_laptop/src/App.js
+++ cilu_laptop/src/App.js
@@ -1,5 +1,5 @@
 import './App.css';
-import Task5Example2ContactForm from './Day6Components/Task5Example2';
+import ParentTask6Example1 from './Day6Components/Task6Example1';
 
 function App() {
   return (
@@ -10,7 +10,8 @@ function App() {
     // <Task4Example1EamilForm/>
     // <Task4Example2PasswordForm/>
     // <Task5Example1RegistrationFrorm/>
-    <Task5Example2ContactForm/>
+    // <Task5Example2ContactForm/>
+    <ParentTask6Example1/>
   );
 }
 

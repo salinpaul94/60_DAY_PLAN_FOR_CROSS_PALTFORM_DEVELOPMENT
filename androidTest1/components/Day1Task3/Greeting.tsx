e238bc460b6961be9e89e9175969e8f894c512d5
salinpaul <salinpv@gamil.com>
2024-09-10 22:45:15 +0530

    day 2 task 3 example 1

---
+++ androidTest1/components/Day1Task3/Greeting.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const Greeting = () => {
+  return (
+    <View>
+      <Text>Custom Greeting Component</Text>
+    </View>
+  );
+};
+
+export default Greeting;

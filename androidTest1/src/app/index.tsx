150b5ab302caec3b60da947820694e3e6a49e2fb
salinpaul <salinpv@gamil.com>
2024-09-12 22:49:48 +0530

    day 2 task 10 example 2

--- androidTest1/src/app/index.tsx
+++ androidTest1/src/app/index.tsx
@@ -4,9 +4,19 @@ import { View, Text, StyleSheet } from 'react-native';
 const App = () => {
   return (
     <View style={ styles.container }>
-      <Text style={ styles.text }>
-        Centered Text
-      </Text>
+      <View style={ styles.box }>
+        <Text style={ styles.text }>Box 1</Text>
+      </View>
+      <View style={ styles.box }>
+        <Text style={ styles.text }>
+          Box 2
+        </Text>
+      </View>
+      <View style={ styles.box }>
+        <Text style={ styles.text }>
+          Box 3
+        </Text>
+      </View>
     </View>
   );
 };
@@ -14,13 +24,18 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    justifyContent: 'space-between',
+    padding: 20,
+  },
+  box: {
+    height: 100,
+    backgroundColor: '#2196F3',
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#4CAF50',
   },
   text: {
     color: '#fff',
-    fontSize: 20,
+    fontSize: 16,
   },
 });
 
